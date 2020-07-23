0d7074f0b344856744fa4e338ba56d3302d068c5
Megan Simmons <m24simmons@gmail.com>
2020-07-23 17:03:57 -0500

    Initial Commit

---
+++ ShoppingList1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList1
+//
+//  Created by Student on 7/23/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
